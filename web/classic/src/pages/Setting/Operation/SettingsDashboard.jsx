2edd76b5cc39b9c38c085e8d0cc39b4a471a1868
samuel <scholarw@hotmail.com>
2026-09-09 16:25:14 +0800

    feat(model-dashboard): expose operational enable switch

--- web/classic/src/pages/Setting/Operation/SettingsDashboard.jsx
+++ web/classic/src/pages/Setting/Operation/SettingsDashboard.jsx
@@ -13,6 +13,7 @@ export default function SettingsDashboard(props) {
   const { t } = useTranslation();
   const [loading, setLoading] = useState(false);
   const [inputs, setInputs] = useState({
+    'dashboard_setting.model_dashboard_enabled': true,
     'dashboard_setting.rate_limit_dashboard_enabled': true,
     'dashboard_setting.performance_dashboard_enabled': true,
     'dashboard_setting.mysql_dashboard_enabled': true,
@@ -75,37 +76,57 @@ export default function SettingsDashboard(props) {
               type='tertiary'
               style={{ marginBottom: 16, display: 'block' }}
             >
-              {t('控制限流看板、性能看板、MySQL面板是否在侧边栏显示')}
+              {t(
+                '控制模型看板、限流看板、性能看板、MySQL面板是否启用并在侧边栏显示',
+              )}
             </Typography.Text>
             <Row gutter={16}>
-              <Col xs={24} sm={12} md={8} lg={8} xl={8}>
+              <Col xs={24} sm={12} md={8} lg={6} xl={6}>
+                <Form.Switch
+                  field={'dashboard_setting.model_dashboard_enabled'}
+                  label={t('启用模型看板')}
+                  size='default'
+                  checkedText='｜'
+                  uncheckedText='〇'
+                  onChange={handleFieldChange(
+                    'dashboard_setting.model_dashboard_enabled',
+                  )}
+                />
+              </Col>
+              <Col xs={24} sm={12} md={8} lg={6} xl={6}>
                 <Form.Switch
                   field={'dashboard_setting.rate_limit_dashboard_enabled'}
                   label={t('启用限流看板')}
                   size='default'
                   checkedText='｜'
                   uncheckedText='〇'
-                  onChange={handleFieldChange('dashboard_setting.rate_limit_dashboard_enabled')}
+                  onChange={handleFieldChange(
+                    'dashboard_setting.rate_limit_dashboard_enabled',
+                  )}
                 />
               </Col>
-              <Col xs={24} sm={12} md={8} lg={8} xl={8}>
+              <Col xs={24} sm={12} md={8} lg={6} xl={6}>
                 <Form.Switch
                   field={'dashboard_setting.performance_dashboard_enabled'}
                   label={t('启用性能看板')}
                   size='default'
                   checkedText='｜'
                   uncheckedText='〇'
-                  onChange={handleFieldChange('dashboard_setting.performance_dashboard_enabled')}
+                  onChange={handleFieldChange(
+                    'dashboard_setting.performance_dashboard_enabled',
+                  )}
                 />
               </Col>
-              <Col xs={24} sm={12} md={8} lg={8} xl={8}>
+              <Col xs={24} sm={12} md={8} lg={6} xl={6}>
                 <Form.Switch
                   field={'dashboard_setting.mysql_dashboard_enabled'}
                   label={t('启用MySQL面板')}
                   size='default'
                   checkedText='｜'
                   uncheckedText='〇'
-                  onChange={handleFieldChange('dashboard_setting.mysql_dashboard_enabled')}
+                  onChange={handleFieldChange(
+                    'dashboard_setting.mysql_dashboard_enabled',
+                  )}
                 />
               </Col>
             </Row>
